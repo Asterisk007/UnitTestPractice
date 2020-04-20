a93a05521025916cd1f32dd1836ab079d2058df3
Asterisk007 <spykid1998@gmail.com>
2020-04-20 11:40:43 -0700

    Add three additional unit tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -20,3 +20,27 @@ TEST(PracticeTest, is_simple_palindrome)
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
 }
+
+// Testing longer string
+TEST(PracticeTest, is_complex_palindrome)
+{
+	Practice obj;
+    bool actual = obj.isPalindrome("qneivnqwpxnwoxcdngusnmepbjbpemnsugndcxownxpwqnvienq");
+    ASSERT_TRUE(actual);
+}
+
+// Testing random input, not a palindrome
+TEST(PracticeTest, is_not_a_randomized_palindrome)
+{
+	Practice obj;
+    bool actual = obj.isPalindrome("kjenfjsdfighakledfjnioaf");
+    ASSERT_FALSE(actual);
+}
+
+// Testing precise input, not a palindrome
+TEST(PracticeTest, is_not_a_palindrome)
+{
+	Practice obj;
+    bool actual = obj.isPalindrome("whatsthistomfooleryyreloofasdatstahw");
+    ASSERT_FALSE(actual);
+}
